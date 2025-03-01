0f3aab4d69097fb40d3f692c9d94b29734faae05
dev-raj05 <dev.dhanak@medront.com>
2025-03-01 11:46:34 +0530

    dynamic tag route bug fixed

--- .netlify/build/chunks/page-ssr_BP2uGixO.mjs
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-const sanityClient = createClient(
-            {"apiVersion":"v2023-08-24","projectId":"f9zwkku2","dataset":"production","useCdn":false,"visualEditing":true,"stega":{"enabled":false,"studioUrl":"\u002Fstudio"}}
-          );
-
-globalThis.sanityClient = sanityClient;
-
-export { sanityClient as s };
--- .netlify/build/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BBzmkga8.mjs';
-export { renderers } from '../renderers.mjs';
