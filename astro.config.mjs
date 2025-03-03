e067880ee2bda97bbea627482bbb82a13190b3ec
dev-raj05 <dev.dhanak@medront.com>
2025-03-03 18:18:57 +0530

    favicon change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 import preact from "@astrojs/preact";
 import { loadEnv } from "vite";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-
 import netlify from "@astrojs/netlify";
 
 const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
