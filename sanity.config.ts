3ca1feb4fc4232e6735618ad8d9e74900a4e84ce
dev-raj05 <dev.dhanak@medront.com>
2025-03-05 14:08:51 +0530

    new

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,9 +11,7 @@ export default defineConfig({
   dataset: "production",
   name: "default",
   title: "astro-env-studio",
-  project: {
-    basePath: "/admin",
-  },
+
   api: {
     projectId: "f9zwkku2",
     dataset: "production",
